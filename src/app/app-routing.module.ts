bd31d0a2e50c42719b04498c717281dd25dbcd76
Gabriel Moura <88740686+mouraus@users.noreply.github.com>
2022-08-01 16:47:13 -0300

    Cria a navbar e carrossel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaInicialComponent } from './paginas/pagina-inicial/pagina-inicial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PaginaInicialComponent },
+  { path: '*', component: PaginaInicialComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
