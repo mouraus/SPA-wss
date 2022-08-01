f892e704b8b158b7c7843fa6edc8d6266dd3d496
Gabriel Moura <88740686+mouraus@users.noreply.github.com>
2022-08-01 14:42:45 -0300

    Conserta o deploy no heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + './dist/wss-app'));
+
+app.get('/*', (req, res)=>{
+    res.sendFile("./dist/wss-app/index.html")
+})
+
+
+app.listen(PORT);
