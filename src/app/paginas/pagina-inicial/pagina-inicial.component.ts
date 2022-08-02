0ce20a8747b91d5ce1a075837a642f4a185f0765
Gabriel Moura <88740686+mouraus@users.noreply.github.com>
2022-08-02 03:22:53 -0300

    Cria seção de categorias e patrocinadores

--- src/app/paginas/pagina-inicial/pagina-inicial.component.ts
+++ src/app/paginas/pagina-inicial/pagina-inicial.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaInicialComponent implements OnInit {
 
+  public patrocinadores: Array<string> = ['../../../assets/patrocinadores/fc-patrocinador.jpeg', '../../../assets/patrocinadores/giga_logo_header.png', '../../../assets/patrocinadores/logo_megatron.png', '../../../assets/patrocinadores/patrocinador-AGL-logo.webp']
+  public categorias: Array<{ titulo: string, img: string }> = [
+    {
+      titulo: 'cameras',
+      img: '../../../assets/categorias/camera.PNG'
+    }, {
+      titulo: 'fechaduras',
+      img: '../../../assets/categorias/fechadura.png'
+    }, {
+      titulo: 'sensores',
+      img: '../../../assets/categorias/sensor.png'
+    }, {
+      titulo: 'interfones',
+      img: '../../../assets/categorias/interfone.png'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
