e9481096eaaac5c67d63692b754872106709985e
Gabriel Moura <88740686+mouraus@users.noreply.github.com>
2022-08-16 13:58:52 -0300

    Cria Pagina do Produto

---
+++ src/app/paginas/pagina-produto/pagina-produto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-produto',
+  templateUrl: './pagina-produto.component.html',
+  styleUrls: ['./pagina-produto.component.scss']
+})
+export class PaginaProdutoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
