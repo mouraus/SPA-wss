806131645e5d65448d7b1c94f9b15471ba0eca49
Gabriel Moura <88740686+mouraus@users.noreply.github.com>
2022-09-03 13:44:12 -0300

    Cria modelo padrao de requisicao e loga no console os produtos

---
+++ src/app/servicos/produtos.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutosService {
+
+ private BASE_URL :string = "https://wss-dev.herokuapp.com"
+
+  constructor(private http :HttpClient) { }
+
+  public getProdutos(){
+    const httpHeaders = new HttpHeaders({
+      'content-type':'application/json',
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwibm9tZSI6IkFkbWluIiwiaWF0IjoxNjYyMTIzMjg0LCJleHAiOjE2NjIxMzA0ODR9.0wSZp1E2gnsI1IJQptF05Q28fVRxad-JHCg-hUdtyL0'
+    })
+    return this.http.get(`${this.BASE_URL}/produtos`,{ headers: httpHeaders })
+  }
+}
