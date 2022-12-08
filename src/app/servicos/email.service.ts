487bc489c837343119439ffe988ebc4160084781
mouraus <g.gmoura@outlook.com>
2022-12-08 19:39:24 -0300

    Implementa envio email

--- src/app/servicos/email.service.ts
+++ src/app/servicos/email.service.ts
@@ -6,18 +6,19 @@ import { ContatoModel } from '../paginas/contato/contato.model';
   providedIn: 'root'
 })
 export class EmailService {
-
+  private storage: any = localStorage.getItem('jwt')
+  private jwt: any = JSON.parse(this.storage)
+  private httpHeaders = new HttpHeaders(
+    {
+      'content-type': 'application/json',
+      'Authorization': "Bearer " + this.jwt.token
+    }
+  )
   private BASE_URL :string = "https://wss-dev.herokuapp.com";
-
-  private HTTP_HEADER_NO_AUTH: HttpHeaders = new HttpHeaders({
-    'content-type':'application/json'
-  })
-
   constructor(private http :HttpClient, private router: Router) { }
 
   public enviarEmail(email: ContatoModel){
-    console.log(email);
-    // return this.http.post<ContatoModel>(`${this.BASE_URL}`,email,{ headers: this.HTTP_HEADER_NO_AUTH })
+    return this.http.post<ContatoModel>(`${this.BASE_URL}`,email,{ headers: this.httpHeaders })
   }
 
 
